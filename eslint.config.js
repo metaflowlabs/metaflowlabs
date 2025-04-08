322b64b0124a10f67c8116b2e4a3b4bc487631c5
Bryan Galvin <bcgalvin@gmail.com>
2025-04-07 22:46:59 -0500

    chore(nuxt): configure unocss/eslint

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+// @ts-check
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+    {
+        stylistic: true,
+        unocss: true,
+        formatters: true,
+        pnpm: true,
+    },
+)
