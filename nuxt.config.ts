43bc183a6f045983c5c10ed92904317cfd7543ed
Bryan Galvin <bcgalvin@gmail.com>
2025-04-07 21:42:56 -0500

    feat(nuxt): configure project for nuxt 4 compatibility

---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-04-07', // Updated compatibility date
+  devtools: { enabled: true },
+
+  // Enable Nuxt 4 compatibility mode
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/test-utils'
+  ]
+})
